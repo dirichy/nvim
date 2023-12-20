9df8552b8a4a6a0f64a229bd590062008e7f453a
dirichy <144102919+dirichy@users.noreply.github.com>
2023-12-20 18:12:02 +0800

    auto enable/disable autopairs

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,3 +2,9 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 local autocmd = vim.api.nvim_create_autocmd
+autocmd("BufEnter", {
+  pattern = "*.tex",
+  callback = function()
+    vim.g.minipairs_disable = true
+  end,
+})
