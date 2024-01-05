fcd0d7a37f6c4a09886c4d227ffe5e09a260f061
dirichy <144102919+dirichy@users.noreply.github.com>
2024-01-05 23:53:32 +0800

    im

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,10 +1,37 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local tex = require("util.conditions")
 local autocmd = vim.api.nvim_create_autocmd
 autocmd("BufEnter", {
   pattern = "*.tex",
   callback = function()
     vim.g.minipairs_disable = true
+  end,
+})
+autocmd("CursorMovedI", {
+  pattern = "*.tex",
+  callback = function()
+    if tex.in_text() then
+      vim.cmd("!fcitx-remote -c")
+    else
+      vim.cmd("!fcitx-remote -o")
+    end
+  end,
+})
+autocmd("InsertLeave", {
+  pattern = "*.tex",
+  callback = function()
+    vim.cmd("!fcitx-remote -o")
+  end,
+})
+autocmd("InsertEnter", {
+  pattern = "*.tex",
+  callback = function()
+    if tex.in_text() then
+      vim.cmd("!fcitx-remote -c")
+    else
+      vim.cmd("!fcitx-remote -o")
+    end
   end,
 })
