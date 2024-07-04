a2cfe90a5e346471a1ef5746b9a2e91fc71130ee
dirichy <144102919+dirichy@users.noreply.github.com>
2024-07-04 21:12:32 +0800

    update

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.cmd([[set exrc]])
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua;"
+package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua;"
